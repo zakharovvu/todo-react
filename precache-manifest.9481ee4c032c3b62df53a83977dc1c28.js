542bbd9709ec5bdbc8925354fbbf1de69c1d421f
Zakharov Viacheslav <zakharovvu@gmail.com>
2019-02-27 11:59:56 +0200

    Updates

---
+++ precache-manifest.9481ee4c032c3b62df53a83977dc1c28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ce964f59e5ce78de96ba",
+    "url": "/todo-react/static/css/main.082e7029.chunk.css"
+  },
+  {
+    "revision": "ce964f59e5ce78de96ba",
+    "url": "/todo-react/static/js/main.ce964f59.chunk.js"
+  },
+  {
+    "revision": "3a890a3f15edfdf19ca0",
+    "url": "/todo-react/static/js/runtime~main.3a890a3f.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/todo-react/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todo-react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a0a12833b929c4bd8ba91708dff1b598",
+    "url": "/todo-react/index.html"
+  }
+];
